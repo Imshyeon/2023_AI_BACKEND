05f428f1774c7f19a24896e84cf2990cf5e9b9f5
suhyeon <kzoen0040@naver.com>
2023-08-26 18:50:01 +0900

    finish

--- pyweb/mysite/mysite/blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-
-class PostForm(ModelForm):
-    class Meta: #key-value
-        model = Post
-        fields = ['title', 'content', 'published_at', 'author'] #key
--- pyweb/mysite/mysite/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/',views.sign_in,name="login"),
-    path('logout/',views.sign_out, name='logout'),
-    path('register/',views.sign_up, name="register"),
-    path('find_account/',views.find_me, name="find_account"),
-]
